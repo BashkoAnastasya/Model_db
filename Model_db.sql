f6c6a4faaaf021c768b82bed8a8d08daf870f079
Anastasiya Bashko <anastasiya.bashko@intexsoft.by>
2018-08-15 15:44:18 +0300

    Add new tables

--- Model_db.sql
+++ Model_db.sql
@@ -1,25 +1,34 @@
-/*  drop table t_ware;
-  drop table t_price_ware;
-  drop table t_supplier;
+/*  drop table t_ware;  
   drop table t_supply;
   drop table t_supply_str;
   drop table t_model;
   drop table t_prace_model;
-  drop table t_ctl_node;*/
+  drop table t_ctl_node;
+  drop table t_supplier; 
+  drop table t_price_ware;
+  drop table  t_dept;
+  drop table t_client;
+  drop table t_sale;
+  drop table t_state;*/
   
-  --поставщик
+  --
   CREATE TABLE t_supplier 
   (
-  id_supplier   number not null,
+  id_supplier  number not null,
   moniker varchar2(254),
-  name varchar2(254)
+  name varchar2(254),
+  constraint pk_id_supplier PRIMARY KEY (id_supplier),
+  constraint t_supplier_unique UNIQUE (moniker)
   );
-  create index IXPK_id_supplier on t_supplier(id_supplier) ;      
-  alter table t_supplier
-  add constraint PK_id_supplier primary key (id_supplier) using index IXPK_id_supplier;      
-  alter table t_supplier
-  add constraint t_supplier_unique UNIQUE (moniker);
-      
+  
+  --состояния 
+  CREATE TABLE t_state
+  (
+  id_state number not null,
+  name_state varchar2(40),
+  constraint pk_id_state primary key (id_state)
+  );
+  
   --поставка
   CREATE TABLE t_supply
   (
@@ -28,27 +37,26 @@
   num varchar2(30),
   dt date,
   id_supplier  number not null,
-  --e_state  enum,
+  id_state  number not null,
   summa number(14,2),
-  nds number(14,2)
+  nds number(14,2),
+  constraint PK_id_supply primary key (id_supply),
+  constraint FK_id_supplier foreign key (id_supplier) references  t_supplier(id_supplier),
+  constraint FK_id_state foreign key (id_state) references t_state(id_state)
   );     
-  create index IXPK_id_supply on t_supply(id_supply) ;   
-  alter table t_supply
-  add constraint PK_id_supply primary key (id_supply) using index IXPK_id_supply
-  add constraint  FK_id_supplier foreign key (id_supplier) references  t_supplier(id_supplier);
-    
+  
+     
   --узел каталога
   CREATE TABLE t_ctl_node
   (
-  id_ctl_node   number not null,
-  id_parent number not null,
+  id_ctl_node  number not null,
+  id_parent number,
   code varchar2(12),
   tree_code varchar2(240),
-  name varchar2(254)
+  name varchar2(254),
+  constraint PK_id_ctl_node primary key (id_ctl_node),
+  constraint FK_id_parent foreign key (id_parent) references t_ctl_node(id_ctl_node)   
   );
-  create index IXPK_id_ctl_node on t_ctl_node(id_ctl_node);
-  alter table t_ctl_node
-  add constraint PK_id_ctl_node primary key (id_ctl_node) using index IXPK_id_ctl_node;
       
   --модель 
   CREATE TABLE t_model
@@ -60,13 +68,11 @@
   grp varchar2(254),
   subgrp varchar2(254),
   label varchar2(254),
-  price number(8,2)    
+  price number(8,2),    
+  constraint PK_id_model primary key (id_model),
+  constraint FK_id_ctl_node foreign key(id_ctl_node)  references t_ctl_node (id_ctl_node)
   );
-  create index IXPK_id_model on t_model(id_model);
-  alter table t_model
-  add constraint PK_id_model primary key (id_model) using index IXPK_id_model
-  add constraint FK_id_ctl_node foreign key(id_ctl_node)  references t_ctl_node (id_ctl_node);
-         
+     
   -- цена модели
   CREATE TABLE t_prace_model
   (
@@ -74,12 +80,11 @@
   id_model   number not null,
   dt_beg date,
   dt_end date,
-  price  number(8,2)   
+  price  number(8,2),
+  constraint FK_id_model foreign key (id_model)references t_model(id_model)  
   );
   create index IX_id_prace_model on t_prace_model(id_prace_model);
-  alter table t_prace_model 
-  add constraint FK_id_model foreign key (id_model) references t_model(id_model);
-   
+    
   --товар
   CREATE TABLE t_ware
   (
@@ -89,12 +94,10 @@
   id_model number not null,
   sz_orig varchar2(30),
   sz_rus varchar2(30),
-  price number(8,2)
+  price number(8,2),
+  constraint PK_id_ware primary key (id_ware),
+  constraint FK_id_model_ware foreign key (id_model) references t_model(id_model)
   );
-  create index IXPK_id_ware on t_ware(id_ware);
-  alter table t_ware
-  add constraint PK_id_ware primary key (id_ware) using index IXPK_id_ware
-  add constraint FK_id_model_ware foreign key (id_model) references t_model(id_model);
     
   --цена товара
   CREATE TABLE t_price_ware
@@ -103,13 +106,13 @@
   id_ware   number not null,
   dt_beg date not null,
   dt_end date,
-  price number(8,2)
+  price number(8,2),
+  constraint FK_id_ware foreign key (id_ware) references t_ware(id_ware)
   );
   create index IX_id_price_ware on t_price_ware(id_price_ware);
-  alter table t_price_ware
-  add constraint FK_id_ware foreign key (id_ware) references t_ware(id_ware);
-  
-   --строка поставки
+
+
+  --строка поставки
   CREATE TABLE t_supply_str
   (
   id_supply_str   number not null,
@@ -118,13 +121,73 @@
   id_ware number not null,
   qty number(6),  
   summa number(14,2),
-  nds number(14,2)
+  nds number(14,2),
+  constraint PK_id_supply_str primary key (id_supply_str),
+  constraint FK_id_supply foreign key (id_supply) references  t_supply(id_supply),
+  constraint FK_id_ware_str foreign key (id_ware) references t_ware(id_ware)
   );
-  create index IXPK_id_supply_str on t_supply_str(id_supply_str);
-  alter table t_supply_str
-  add constraint PK_id_supply_str primary key (id_supply_str) using index IXPK_id_supply_str
-  add constraint FK_id_supply foreign key (id_supply) references  t_supply(id_supply)
-  add constraint FK_id_ware_str foreign key (id_ware) references t_ware(id_ware) ;
+  
+  -- подразделение 
+  create table t_dept
+  (
+  id_dept number  not null,
+  name  varchar2(254),
+  id_parent number, 
+  constraint PK_id_dept primary key (id_dept), 
+  constraint FK_id_parent_dept foreign key (id_parent) references t_dept (id_dept)
+  );
+  
+  --клиент 
+  create table t_client
+  (
+  id_client number  not null,
+  id_dept number  not null,
+  moniker varchar2(254),
+  name varchar2(254),
+  is_vip char(1) check (is_vip in ('Y','N')),
+  town varchar2(254),
+  constraint PK_id_client primary key (id_client),
+  constraint FK_id_dept_client foreign key (id_dept) references t_dept (id_dept),
+  constraint t_client_unique UNIQUE (moniker)
+  );
+  
+  --продажа 
+  create table t_sale
+  (
+  id_sale number  not null,
+  num varchar2(30),
+  dt date,
+  id_client number  not null, 
+  id_state number  not null,
+  discount number (8,6),
+  summa number(14,2),
+  nds number (14,2),
+  constraint PK_id_sale primary key (id_sale),
+  constraint FK_id_client foreign key (id_client) references t_client(id_client),
+  constraint FK_id_state_sale foreign key (id_state) references t_state(id_state)
+  );
+  
+  -- строка продажи 
+  create table t_sale_str
+  (
+  id_sale_str number  not null,
+  id_sale number  not null,
+  num number(6),
+  id_ware number  not null,
+  qty number(6),
+  price number(8,2), 
+  discount number (8,6), 
+  disc_price number (8,2),
+  summa number (14,2), 
+  nds number (14,2),
+  constraint PK_id_sale_str primary key (id_sale_str),
+  constraint FK_id_sale foreign key (id_sale) references t_sale(id_sale),
+  constraint FK_id_ware_sale foreign key (id_ware) references t_ware(id_ware)  
+  );
+  
+  
+
+ 
       
       
       
