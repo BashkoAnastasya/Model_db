829a1480392664d77f4a867d13db99d2dfc5b8b5
INTEXSOFT\anastasiya.bashko <nastinm@mail.ru>
2018-08-20 17:37:56 +0300

    Add new procedure

--- Records.sql
+++ Records.sql
@@ -5,7 +5,7 @@ insert into t_supplier VALUES (3,'IP Berilov','Berilov I I');
 insert into t_state VALUES (1,'Draft');
 insert into t_state values  (2,'Fulfilled');
 
-insert into t_supply (id_supply, code, dt, id_supplier,id_state) values  (1,'DB17',DATE'2018-08-14',1,2);
+insert into t_supply (id_supply, code, dt, id_supplier,id_state) values  (1,'DB17',DATE'2018-08-14',1,1);
 insert into t_supply (id_supply, code, dt, id_supplier,id_state) values  (2,'DB18',DATE'2018-08-15',2,1);
 insert into t_supply (id_supply, code, dt, id_supplier,id_state) values  (3,'DB19',DATE'2018-08-16',3,1);
 
@@ -22,13 +22,17 @@ insert into t_price_model (id_price_model,id_model,dt_beg,dt_end,price) values (
 insert into t_price_model (id_price_model,id_model,dt_beg,dt_end,price) values (2,2,DATE'2016-05-14',DATE'2019-05-14',888);
 insert into t_price_model (id_price_model,id_model,dt_beg,dt_end,price) values (3,3,DATE'2015-03-14',DATE'2019-05-14',1212);
 
-insert into t_ware (id_ware,moniker,name,id_model,sz_orig,sz_rus) values (1,'Armaruta25','Armaruta25',2,'dfsd','fasf');
-insert into t_ware (id_ware,moniker,name,id_model,sz_orig,sz_rus) values (2,'Armaruta26','Armaruta26',2,'sfsd','sdfas');
-insert into t_ware (id_ware,moniker,name,id_model,sz_orig,sz_rus) values (3,'Steel','Steel1',3,'gfsdfg','df');
+insert into t_ware (id_ware,moniker,name,id_model,sz_orig,sz_rus) values (1,'Bath Armaruta25','Armaruta25',2,'dfsd','fasf');
+insert into t_ware (id_ware,moniker,name,id_model,sz_orig,sz_rus) values (2,'Bath Armaruta26','Armaruta26',2,'sfsd','sdfas');
+insert into t_ware (id_ware,moniker,name,id_model,sz_orig,sz_rus) values (3,'Bath Steel','Steel1',3,'gfsdfg','df');
 
-insert into t_supply_str (id_supply_str, id_supply, num, id_ware, qty, price) values  (1, 1, 1, 1, 2, 20);
-insert into t_supply_str (id_supply_str, id_supply, num, id_ware, qty, price) values  (2, 1, 2, 2, 4, 5);
-insert into t_supply_str (id_supply_str, id_supply, num, id_ware, qty, price) values  (3, 1, 3, 3, 10, 6);
+insert into t_price_ware (id_price_ware,id_ware,dt_beg,dt_end,price) values (sec_t_price_ware.nextval,1,DATE'2018-08-15',null,18);
+insert into t_price_ware (id_price_ware,id_ware,dt_beg,dt_end,price) values (sec_t_price_ware.nextval,2,DATE'2018-08-15',null,15);
+insert into t_price_ware (id_price_ware,id_ware,dt_beg,dt_end,price) values (sec_t_price_ware.nextval,3,DATE'2018-08-15',null,7);
+
+insert into t_supply_str (id_supply_str, id_supply, numa, id_ware, qty, price) values  (1, 1, 1, 1, 2, 20);
+insert into t_supply_str (id_supply_str, id_supply, numa, id_ware, qty, price) values  (2, 1, 2, 2, 4, 5);
+insert into t_supply_str (id_supply_str, id_supply, numa, id_ware, qty, price) values  (3, 1, 3, 3, 10, 6);
 
 insert into t_dept (id_dept,name,id_parent) values (1,'Global',Null);
 insert into t_dept (id_dept,name,id_parent) values (2,'Vip',1);
@@ -41,7 +45,7 @@ insert into t_client (id_client,id_dept,moniker,name,is_vip,town) values(3,3,'Pe
 
 insert into t_sale (id_sale,num,dt,id_client,id_state,discount) values (1,1,DATE'2018-08-18',1,1,10);
 insert into t_sale (id_sale,num,dt,id_client,id_state,discount) values (2,2,DATE'2018-08-19',2,1,10);
-insert into t_sale (id_sale,num,dt,id_client,id_state,discount) values (3,1,DATE'2018-08-20',3,2,10);
+insert into t_sale (id_sale,num,dt,id_client,id_state,discount) values (3,1,DATE'2018-08-20',3,1,10);
 
 insert into t_sale_str (id_sale_str,id_sale,num,id_ware,qty,discount) values(1,1,1,1,10,2);
 insert into t_sale_str (id_sale_str,id_sale,num,id_ware,qty,discount) values(2,1,2,2,5,2);
--- Triggers.sql
+++ Triggers.sql
@@ -1,71 +1,14 @@
-
---триггер для расчета суммы и ндс в таблице t_supply_str
-create or replace noneditionable trigger Tsumma_t_supply
-before update or delete or insert on t_supply_str
-for each row
-begin
-   :new.summa   := :new.price * :new.qty;
-   :new.nds     := :new.price * :new.qty*18/118;
-    pkg_around_mutation.bUpdPainters := true; 
-        
- if (inserting or updating) then
-    pkg_around_mutation.id_supply:=:new.id_supply; 
- else
-     pkg_around_mutation.id_supply:=:old.id_supply; 
- end if;    
-end Tsumma_t_supply;
-
---триггер для расчета суммы в таблице t_supply
-create or replace noneditionable trigger tr_turtles_bu
-after update or delete or insert
-on t_supply_str
-begin
-  pkg_around_mutation.update_painters;
-end tr_turtles_bu;
-
--- пакет для обхода мутации таблицы
-create or replace noneditionable package pkg_around_mutation
-is
-  bUpdPainters boolean;
-  id_supply number;
-  procedure update_painters;
-end pkg_around_mutation;
-
---процедура для обхода мутации таблиц
-create or replace noneditionable package body pkg_around_mutation
-is
-  procedure update_painters
-  is
-  summa t_supply.summa%TYPE;
-  begin
-    if bUpdPainters then
-       bUpdPainters := false;
-
-      select sum(t.summa)
-        into summa
-        from t_supply_str t
-       where t.id_supply = 1;
-
-      update t_supply ts
-         set ts.summa = summa
-      where ts.id_supply = 1;
-    end if;
-  end;
-end pkg_around_mutation;
-
-
---TASK-02
-CREATE OR REPLACE NONEDITIONABLE TRIGGER changes_discount
+CREATE OR REPLACE  TRIGGER changes_discount
   before update or delete or insert on t_sale
   for each row
-    
+
 declare
   t_is_vip t_client.is_vip%TYPE;
-  no_sales  EXCEPTION;
+  no_sales   EXCEPTION;
   no_changes EXCEPTION;
-  
+
 BEGIN
-  if :old.id_state = 1  or :old.id_state is null then
+  if :old.id_state = 1 or :old.id_state is null then
   
     select t_client.is_vip
       into t_is_vip
@@ -77,8 +20,14 @@ BEGIN
     end if;
   else
     RAISE no_changes;
-  end if;
+  end if;  
   
+/*  if updating then
+    update t_sale_str t
+       set t.disc_price = t.price * t.discount * :new.discount
+     where t.id_sale = :new.id_sale;
+  end if;*/   
+
 EXCEPTION
   WHEN no_sales THEN
     raise_application_error(-20001, 'Sale no more 20%');
@@ -86,8 +35,7 @@ EXCEPTION
     raise_application_error(-20002, 'No changes actual documets%');
 END;
 
---TASK-03
-CREATE OR REPLACE NONEDITIONABLE TRIGGER price_sale
+CREATE OR REPLACE  TRIGGER price_sale
   before update or insert on t_sale_str
   for each row
 
@@ -103,23 +51,123 @@ BEGIN
    where t.id_sale = :new.id_sale;
 
   if t_id_state = 1 then
-    select tpw.price * ts.discount / 100
+    select tpw.price
       into t_price
       from t_price_ware tpw, t_sale ts
      where tpw.id_ware = :new.id_ware
        and ts.id_sale = :new.id_sale
        and ts.dt >= tpw.dt_beg
        and (ts.dt < tpw.dt_end or tpw.dt_end is null);
-    :new.price := t_price;
+    :new.price := t_price;        
   else
-    RAISE no_changes;
+    RAISE no_changes;    
   end if;
 
 EXCEPTION
   WHEN no_changes THEN
     raise_application_error(-20002, 'No changes actual documets%');
 END;
+create or replace  trigger summa_sale_str
+  before update or insert on t_sale_str
+  for each row
+begin
+  
 
 
+  :new.disc_price := :new.price - (:new.price * :new.discount / 100);
+  :new.summa      := (:new.price -
+                     (:new.price * :new.discount / 100)) * :new.qty;
+  :new.nds        := (:new.price -
+                     (:new.price * :new.discount / 100)) * :new.qty * 118 / 100;
+
+  pkg_around_mutation.bUpdPainters := true;
+  pkg_around_mutation.t_id_sale    := :new.id_sale;
+
+end;
+
+
+create or replace  trigger T_summa_t_supply
+after update or delete or insert
+on t_supply_str
+begin
+  pkg_around_mutation.p_summa_t_supply;
+end T_summa_t_supply;
+
+create or replace  trigger T_summa_t_supply_str
+  before update or delete or insert on t_supply_str
+  for each row
+
+begin
+   
+    :new.summa                       := :new.price  * :new.qty;
+    :new.nds                         := :new.price * :new.qty * 18 / 118;
+    pkg_around_mutation.bUpdPainters := true;
+  
+    if (inserting or updating) then
+      pkg_around_mutation.t_id_supply := :new.id_supply;
+    else
+      pkg_around_mutation.t_id_supply := :old.id_supply;
+    end if;
+    change_count_ware(:new.id_ware, :new.qty);
+end Tsumma_t_supply;
+
+create or replace  trigger T_summa_t_sale
+after update or delete or insert
+on t_sale_str
+begin
+  pkg_around_mutation.p_summa_t_sale;
+end ;
+
+create or replace noneditionable package body pkg_around_mutation
+is
+  procedure p_summa_t_supply
+  is
+  t_summa t_supply.summa%TYPE;
+  t_nds t_supply.nds%type;
+  
+  begin
+    if bUpdPainters then
+       bUpdPainters := false;
+
+      select sum(summa),sum(nds)
+        into t_summa,t_nds
+        from t_supply_str z where z.id_supply=t_id_supply;
+
+      update t_supply ts
+         set ts.summa = t_summa, ts.nds=t_nds
+      where ts.id_supply = t_id_supply;
+    end if;
+  end;
+  
+  --процедура для расчета суммы и ндс таблицы t_sale
+  procedure p_summa_t_sale
+  is
+  t_summa_sale t_sale.summa%TYPE;
+  t_nds_sale t_sale.nds%type;
+  
+  begin
+    if bUpdPainters then
+       bUpdPainters := false;
+
+      select sum(summa),sum(nds)
+        into t_summa_sale,t_nds_sale
+        from t_sale_str z where z.id_sale=t_id_sale;
+
+      update t_sale ts
+         set ts.summa = t_summa_sale, ts.nds=t_nds_sale
+      where ts.id_sale =t_id_sale;
+    end if;
+  end;  
+  
+end pkg_around_mutation;
+
+create or replace noneditionable package pkg_around_mutation
+is
+  bUpdPainters boolean;
+  t_id_supply number;
+  t_id_sale number;
+  procedure p_summa_t_supply;
+  procedure p_summa_t_sale;
+end pkg_around_mutation;
 
 
