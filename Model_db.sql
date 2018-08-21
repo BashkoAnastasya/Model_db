cd89a87a9f7c7cb275de20806c1357478351403b
INTEXSOFT\anastasiya.bashko <nastinm@mail.ru>
2018-08-21 17:39:37 +0300

    Изменение оформление

--- Model_db.sql
+++ Model_db.sql
@@ -1,197 +1,196 @@
-/*  drop table t_supply_str;
-  drop table t_sale_str;
-  drop table t_price_ware;
-  drop table t_price_model;  
-  drop table t_ware;  
-  drop table t_supply;  
-  drop table t_supplier; 
-  drop table t_model;
-  drop table t_sale; 
-  drop table t_client;
-  drop table t_ctl_node;
-  drop table t_dept;
-  drop table t_state;*/
+/*  DROP TABLE t_supply_str;
+  DROP TABLE t_sale_str;
+  DROP TABLE t_price_ware;
+  DROP TABLE t_price_model;  
+  DROP TABLE t_ware;  
+  DROP TABLE t_supply;  
+  DROP TABLE t_supplier; 
+  DROP TABLE t_model;
+  DROP TABLE t_sale; 
+  DROP TABLE t_client;
+  DROP TABLE t_ctl_node;
+  DROP TABLE t_dept;
+  DROP TABLE t_state;*/
   
 
   CREATE TABLE t_supplier 
   (
-  id_supplier  number not null,
-  moniker varchar2(254),
-  name varchar2(254),
-  constraint pk_id_supplier PRIMARY KEY (id_supplier),
-  constraint t_supplier_unique UNIQUE (moniker)
+  id_supplier  NUMBER NOT NULL,
+  moniker VARCHAR2(254),
+  name VARCHAR2(254),
+  CONSTRAINT pk_id_supplier PRIMARY KEY (id_supplier),
+  CONSTRAINT t_supplier_unique UNIQUE (moniker)
   );
   
 
   CREATE TABLE t_state
   (
-  id_state number not null,
-  name_state varchar2(40),
-  constraint pk_id_state primary key (id_state)
+  id_state NUMBER NOT NULL,
+  name_state VARCHAR2(40),
+  CONSTRAINT pk_id_state PRIMARY KEY (id_state)
   );
   
 
   CREATE TABLE t_supply
   (
-  id_supply   number not null,
-  code varchar2(30),
-  num varchar2(30),
-  dt date,
-  id_supplier  number not null,
-  id_state  number not null,
-  summa number(14,2),
-  nds number(14,2),
-  constraint PK_id_supply primary key (id_supply),
-  constraint FK_id_supplier foreign key (id_supplier) references  t_supplier(id_supplier),
-  constraint FK_id_state foreign key (id_state) references t_state(id_state)
+  id_supply   NUMBER NOT NULL,
+  code VARCHAR2(30),
+  num VARCHAR2(30),
+  dt DATE,
+  id_supplier  NUMBER NOT NULL,
+  id_state  NUMBER NOT NULL,
+  summa NUMBER (14,2),
+  nds NUMBER (14,2),
+  CONSTRAINT pk_id_supply PRIMARY KEY (id_supply),
+  CONSTRAINT fk_id_supplier FOREIGN KEY (id_supplier) REFERENCES  t_supplier(id_supplier),
+  CONSTRAINT fk_id_state FOREIGN KEY (id_state) REFERENCES t_state(id_state)
   );     
        
 
   CREATE TABLE t_ctl_node
   (
-  id_ctl_node  number not null,
-  id_parent number,
-  code varchar2(12),
-  tree_code varchar2(240),
-  name varchar2(254),
-  constraint PK_id_ctl_node primary key (id_ctl_node),
-  constraint FK_id_parent foreign key (id_parent) references t_ctl_node(id_ctl_node)   
+  id_ctl_node  NUMBER NOT NULL,
+  id_parent NUMBER,
+  code VARCHAR2(12),
+  tree_code VARCHAR2(240),
+  name VARCHAR2(254),
+  CONSTRAINT pk_id_ctl_node PRIMARY KEY (id_ctl_node),
+  CONSTRAINT fk_id_parent FOREIGN KEY (id_parent) REFERENCES t_ctl_node(id_ctl_node)   
   );
       
 
   CREATE TABLE t_model
   (
-  id_model   number not null,
-  moniker varchar2(12),
-  name varchar2(254),
-  id_ctl_node   number not null,
-  grp varchar2(254),
-  subgrp varchar2(254),
-  label varchar2(254),
-  price number(8,2),    
-  constraint PK_id_model primary key (id_model),
-  constraint FK_id_ctl_node foreign key(id_ctl_node)  references t_ctl_node (id_ctl_node)
+  id_model   NUMBER NOT NULL,
+  moniker VARCHAR2(12),
+  name VARCHAR2(254),
+  id_ctl_node   NUMBER NOT NULL,
+  grp VARCHAR2(254),
+  subgrp VARCHAR2(254),
+  label VARCHAR2(254),
+  price NUMBER(8,2),    
+  CONSTRAINT pk_id_model PRIMARY KEY (id_model),
+  CONSTRAINT fk_id_ctl_node FOREIGN KEY(id_ctl_node)  REFERENCES t_ctl_node (id_ctl_node)
   );
     
 
   CREATE TABLE t_price_model
   (
-  id_price_model number not null,
-  id_model   number not null,
-  dt_beg date,
-  dt_end date,
-  price  number(8,2),
-  constraint FK_id_model foreign key (id_model)references t_model(id_model)  
+  id_price_model NUMBER NOT NULL,
+  id_model   NUMBER NOT NULL,
+  dt_beg DATE,
+  dt_end DATE,
+  price  NUMBER(8,2),
+  CONSTRAINT fk_id_model FOREIGN KEY (id_model)REFERENCES t_model(id_model)  
   );
-  create index IX_id_price_model on t_price_model(id_price_model);
+  CREATE INDEX ix_id_price_model on t_price_model(id_price_model);
     
 
   CREATE TABLE t_ware
   (
-  id_ware number not null,
-  moniker varchar2(12),
-  name varchar2 (254),
-  id_model number not null,
-  sz_orig varchar2(30),
-  sz_rus varchar2(30),
-  price number(8,2),
-  constraint PK_id_ware primary key (id_ware),
-  constraint FK_id_model_ware foreign key (id_model) references t_model(id_model)
+  id_ware NUMBER NOT NULL,
+  moniker VARCHAR2(12),
+  name VARCHAR2 (254),
+  id_model NUMBER NOT NULL,
+  sz_orig VARCHAR2(30),
+  sz_rus VARCHAR2(30),
+  price NUMBER(8,2),
+  CONSTRAINT pk_id_ware PRIMARY KEY (id_ware),
+  CONSTRAINT fk_id_model_ware FOREIGN KEY (id_model) REFERENCES t_model(id_model)
   );
     
 
   CREATE TABLE t_price_ware
   (
-  id_price_ware number not null,
-  id_ware   number not null,
-  dt_beg date not null,
-  dt_end date,
-  price number(8,2),
-  constraint FK_id_ware foreign key (id_ware) references t_ware(id_ware)
+  id_price_ware NUMBER NOT NULL,
+  id_ware   NUMBER NOT NULL,
+  dt_beg DATE NOT NULL,
+  dt_end DATE,
+  price NUMBER(8,2),
+  CONSTRAINT fk_id_ware FOREIGN KEY (id_ware) REFERENCES t_ware(id_ware)
   );
-  create index IX_id_price_ware on t_price_ware(id_price_ware);
+  CREATE INDEX ix_id_price_ware on t_price_ware(id_price_ware);
 
 
 
   CREATE TABLE t_supply_str
   (
-  id_supply_str   number not null,
-  id_supply number not null,
-  num number(6),
-  id_ware number not null,
-  qty number(6),  
-  price number (8,2),
-  summa number(14,2),
-  nds number(14,2),
-  constraint PK_id_supply_str primary key (id_supply_str),
-  constraint FK_id_supply foreign key (id_supply) references  t_supply(id_supply),
-  constraint FK_id_ware_str foreign key (id_ware) references t_ware(id_ware)
+  id_supply_str   NUMBER NOT NULL,
+  id_supply NUMBER NOT NULL,
+  num NUMBER(6),
+  id_ware NUMBER NOT NULL,
+  qty NUMBER(6),  
+  price NUMBER (8,2),
+  summa NUMBER (14,2),
+  nds NUMBER(14,2),
+  CONSTRAINT pk_id_supply_str PRIMARY KEY (id_supply_str),
+  CONSTRAINT fk_id_supply FOREIGN KEY (id_supply) REFERENCES  t_supply(id_supply),
+  CONSTRAINT fk_id_ware_str FOREIGN KEY (id_ware) REFERENCES t_ware(id_ware)
   );
   
  
-  create table t_dept
+  CREATE TABLE t_dept
   (
-  id_dept number  not null,
-  name  varchar2(254),
-  id_parent number, 
-  constraint PK_id_dept primary key (id_dept), 
-  constraint FK_id_parent_dept foreign key (id_parent) references t_dept (id_dept)
+  id_dept NUMBER NOT NULL,
+  name  VARCHAR2(254),
+  id_parent NUMBER, 
+  CONSTRAINT pk_id_dept PRIMARY KEY (id_dept), 
+  CONSTRAINT fk_id_parent_dept FOREIGN KEY (id_parent) REFERENCES t_dept (id_dept)
   );
   
   
-  create table t_client
+  CREATE TABLE t_client
   (
-  id_client number  not null,
-  id_dept number  not null,
-  moniker varchar2(254),
-  name varchar2(254),
-  is_vip char(1) check (is_vip in ('Y','N')),
-  town varchar2(254),
-  constraint PK_id_client primary key (id_client),
-  constraint FK_id_dept_client foreign key (id_dept) references t_dept (id_dept),
-  constraint t_client_unique UNIQUE (moniker)
-  );
-  
- 
-  create table t_sale
-  (
-  id_sale number  not null,
-  num varchar2(30),
-  dt date,
-  id_client number  not null, 
-  id_state number  not null,
-  discount number (8,6),
-  summa number(14,2),
-  nds number (14,2),
-  constraint PK_id_sale primary key (id_sale),
-  constraint FK_id_client foreign key (id_client) references t_client(id_client),
-  constraint FK_id_state_sale foreign key (id_state) references t_state(id_state)
+  id_client NUMBER NOT NULL,
+  id_dept NUMBER NOT NULL,
+  moniker VARCHAR2(254),
+  name VARCHAR2(254),
+  is_vip CHAR(1) CHECK (is_vip in ('Y','N')),
+  town VARCHAR2(254),
+  CONSTRAINT pk_id_client PRIMARY KEY (id_client),
+  CONSTRAINT fk_id_dept_client FOREIGN KEY (id_dept) REFERENCES t_dept (id_dept),
+  CONSTRAINT t_client_unique UNIQUE (moniker)
   );
   
  
-  create table t_sale_str
+  CREATE TABLE t_sale
   (
-  id_sale_str number  not null,
-  id_sale number  not null,
-  num number(6),
-  id_ware number  not null,
-  qty number(6),
-  price number(8,2), 
-  discount number (8,6), 
-  disc_price number (8,2),
-  summa number (14,2), 
-  nds number (14,2),
-  constraint PK_id_sale_str primary key (id_sale_str),
-  constraint FK_id_sale foreign key (id_sale) references t_sale(id_sale),
-  constraint FK_id_ware_sale foreign key (id_ware) references t_ware(id_ware)  
+  id_sale NUMBER NOT NULL,
+  num VARCHAR2(30),
+  dt DATE,
+  id_client NUMBER NOT NULL, 
+  id_state NUMBER NOT NULL,
+  discount NUMBER (8,6),
+  summa NUMBER(14,2),
+  nds NUMBER (14,2),
+  CONSTRAINT pk_id_sale PRIMARY KEY (id_sale),
+  CONSTRAINT fk_id_client FOREIGN KEY (id_client) REFERENCES t_client(id_client),
+  CONSTRAINT fk_id_state_sale FOREIGN KEY (id_state) REFERENCES t_state(id_state)
   );
   
-  create table t_rest
+ 
+  CREATE TABLE t_sale_str
   (
-  id_t_rest number  not null,
-  id_ware number  not null,
-  qty number(6),
-  constraint PK_id_t_rest primary key (id_t_rest)    
+  id_sale_str NUMBER NOT NULL,
+  id_sale NUMBER NOT NULL,
+  num NUMBER(6),
+  id_ware NUMBER NOT NULL,
+  qty NUMBER(6),
+  price NUMBER(8,2), 
+  discount NUMBER (8,6), 
+  disc_price NUMBER (8,2),
+  summa NUMBER (14,2), 
+  nds NUMBER (14,2),
+  CONSTRAINT pk_id_sale_str PRIMARY KEY (id_sale_str),
+  CONSTRAINT fk_id_sale FOREIGN KEY (id_sale) REFERENCES t_sale(id_sale),
+  CONSTRAINT fk_id_ware_sale FOREIGN KEY (id_ware) REFERENCES t_ware(id_ware)  
+  );
+  
+  CREATE TABLE t_rest
+  (
+  id_ware NUMBER NOT NULL,
+  qty NUMBER(6),
+  CONSTRAINT fk_id_ware_rest FOREIGN KEY (id_ware) REFERENCES t_ware(id_ware)    
   );
   
 
--- Records.sql
+++ Records.sql
@@ -1,56 +1,56 @@
-insert into t_supplier VALUES (1,'IP Frilov','Frilov I I');
-insert into t_supplier VALUES (2,'IP Popov','Popov I I ');
-insert into t_supplier VALUES (3,'IP Berilov','Berilov I I');
+INSERT INTO t_supplier VALUES (1,'IP Frilov','Frilov I I');
+INSERT INTO t_supplier VALUES (2,'IP Popov','Popov I I ');
+INSERT INTO t_supplier VALUES (3,'IP Berilov','Berilov I I');
 
-insert into t_state VALUES (1,'Draft');
-insert into t_state values  (2,'Fulfilled');
+INSERT INTO t_state VALUES (1,'Draft');
+INSERT INTO t_state VALUES  (2,'Fulfilled');
 
-insert into t_supply (id_supply, code, dt, id_supplier,id_state) values  (1,'DB17',DATE'2018-08-14',1,1);
-insert into t_supply (id_supply, code, dt, id_supplier,id_state) values  (2,'DB18',DATE'2018-08-15',2,1);
-insert into t_supply (id_supply, code, dt, id_supplier,id_state) values  (3,'DB19',DATE'2018-08-16',3,1);
+INSERT INTO t_supply (id_supply, code, dt, id_supplier,id_state) VALUES  (1,'DB17',DATE'2018-08-14',1,1);
+INSERT INTO t_supply (id_supply, code, dt, id_supplier,id_state) VALUES  (2,'DB18',DATE'2018-08-15',2,1);
+INSERT INTO t_supply (id_supply, code, dt, id_supplier,id_state) VALUES  (3,'DB19',DATE'2018-08-16',3,1);
 
-insert into t_ctl_node values (1,'','78ds','45','Baths');
-insert into t_ctl_node values (2,1,'sdf5','12','Acrylic Baths');
-insert into t_ctl_node values (3,1,'sdf5','14','Steel Baths');
-insert into t_ctl_node values (4,1,'sdf5','16','Bath screens');
+INSERT INTO t_ctl_node VALUES (1,'','78ds','45','Baths');
+INSERT INTO t_ctl_node VALUES (2,1,'sdf5','12','Acrylic Baths');
+INSERT INTO t_ctl_node VALUES (3,1,'sdf5','14','Steel Baths');
+INSERT INTO t_ctl_node VALUES (4,1,'sdf5','16','Bath screens');
 
-insert into t_model (id_model,moniker,name,id_ctl_node,grp,subgrp,label,price) values (1,'GDj458','Cerazit',2,'Cerazit 258','Cerazit','fd',1);
-insert into t_model (id_model,moniker,name,id_ctl_node,grp,subgrp,label,price) values (2,'JFDJ','Armaruta',3,'Armaruta 258','Armaruta','rt',2);
-insert into t_model (id_model,moniker,name,id_ctl_node,grp,subgrp,label,price) values (3,'REFDS','Braun',4,'Braun 258','Braun','eret',3);
+INSERT INTO t_model (id_model,moniker,name,id_ctl_node,grp,subgrp,label,price) VALUES (1,'GDj458','Cerazit',2,'Cerazit 258','Cerazit','fd',1);
+INSERT INTO t_model (id_model,moniker,name,id_ctl_node,grp,subgrp,label,price) VALUES (2,'JFDJ','Armaruta',3,'Armaruta 258','Armaruta','rt',2);
+INSERT INTO t_model (id_model,moniker,name,id_ctl_node,grp,subgrp,label,price) VALUES (3,'REFDS','Braun',4,'Braun 258','Braun','eret',3);
 
-insert into t_price_model (id_price_model,id_model,dt_beg,dt_end,price) values (1,1,DATE'2017-08-14',DATE'2019-08-14',10);
-insert into t_price_model (id_price_model,id_model,dt_beg,dt_end,price) values (2,2,DATE'2016-05-14',DATE'2019-05-14',888);
-insert into t_price_model (id_price_model,id_model,dt_beg,dt_end,price) values (3,3,DATE'2015-03-14',DATE'2019-05-14',1212);
+INSERT INTO t_price_model (id_price_model,id_model,dt_beg,dt_end,price) VALUES (1,1,DATE'2017-08-14',DATE'2019-08-14',10);
+INSERT INTO t_price_model (id_price_model,id_model,dt_beg,dt_end,price) VALUES (2,2,DATE'2016-05-14',DATE'2019-05-14',888);
+INSERT INTO t_price_model (id_price_model,id_model,dt_beg,dt_end,price) VALUES (3,3,DATE'2015-03-14',DATE'2019-05-14',1212);
 
-insert into t_ware (id_ware,moniker,name,id_model,sz_orig,sz_rus) values (1,'Bath Armaruta25','Armaruta25',2,'dfsd','fasf');
-insert into t_ware (id_ware,moniker,name,id_model,sz_orig,sz_rus) values (2,'Bath Armaruta26','Armaruta26',2,'sfsd','sdfas');
-insert into t_ware (id_ware,moniker,name,id_model,sz_orig,sz_rus) values (3,'Bath Steel','Steel1',3,'gfsdfg','df');
+INSERT INTO t_ware (id_ware,moniker,name,id_model,sz_orig,sz_rus) VALUES (1,'Bath Armaruta25','Armaruta25',2,'dfsd','fasf');
+INSERT INTO t_ware (id_ware,moniker,name,id_model,sz_orig,sz_rus) VALUES (2,'Bath Armaruta26','Armaruta26',2,'sfsd','sdfas');
+INSERT INTO t_ware (id_ware,moniker,name,id_model,sz_orig,sz_rus) VALUES (3,'Bath Steel','Steel1',3,'gfsdfg','df');
 
-insert into t_price_ware (id_price_ware,id_ware,dt_beg,dt_end,price) values (sec_t_price_ware.nextval,1,DATE'2018-08-15',null,18);
-insert into t_price_ware (id_price_ware,id_ware,dt_beg,dt_end,price) values (sec_t_price_ware.nextval,2,DATE'2018-08-15',null,15);
-insert into t_price_ware (id_price_ware,id_ware,dt_beg,dt_end,price) values (sec_t_price_ware.nextval,3,DATE'2018-08-15',null,7);
+INSERT INTO t_price_ware (id_price_ware,id_ware,dt_beg,dt_end,price) VALUES (sec_t_price_ware.nextval,1,DATE'2018-08-15',NULL,18);
+INSERT INTO t_price_ware (id_price_ware,id_ware,dt_beg,dt_end,price) VALUES (sec_t_price_ware.nextval,2,DATE'2018-08-15',NULL,15);
+INSERT INTO t_price_ware (id_price_ware,id_ware,dt_beg,dt_end,price) VALUES (sec_t_price_ware.nextval,3,DATE'2018-08-15',NULL,7);
 
-insert into t_supply_str (id_supply_str, id_supply, numa, id_ware, qty, price) values  (1, 1, 1, 1, 2, 20);
-insert into t_supply_str (id_supply_str, id_supply, numa, id_ware, qty, price) values  (2, 1, 2, 2, 4, 5);
-insert into t_supply_str (id_supply_str, id_supply, numa, id_ware, qty, price) values  (3, 1, 3, 3, 10, 6);
+INSERT INTO t_supply_str (id_supply_str, id_supply, num, id_ware, qty, price) VALUES  (1, 1, 1, 1, 2, 20);
+INSERT INTO t_supply_str (id_supply_str, id_supply, num, id_ware, qty, price) VALUES  (2, 1, 2, 2, 4, 5);
+INSERT INTO t_supply_str (id_supply_str, id_supply, num, id_ware, qty, price) VALUES  (3, 1, 3, 3, 10, 6);
 
-insert into t_dept (id_dept,name,id_parent) values (1,'Global',Null);
-insert into t_dept (id_dept,name,id_parent) values (2,'Vip',1);
-insert into t_dept (id_dept,name,id_parent) values (3,'MNDfn',1);
+INSERT INTO t_dept (id_dept,name,id_parent) VALUES (1,'Global',NULL);
+INSERT INTO t_dept (id_dept,name,id_parent) VALUES (2,'Vip',1);
+INSERT INTO t_dept (id_dept,name,id_parent) VALUES (3,'MNDfn',1);
 
-insert into t_client (id_client,id_dept,moniker,name,is_vip,town) values(1,2,'Ivanov','Ivanov i i', 'Y', 'Grodno');
-insert into t_client (id_client,id_dept,moniker,name,is_vip,town) values(2,3,'Sidorov','Sidorov i i', 'N', 'Grodno');
-insert into t_client (id_client,id_dept,moniker,name,is_vip,town) values(3,3,'Petrov','Petrov i i', 'N', 'Grodno');
+INSERT INTO t_client (id_client,id_dept,moniker,name,is_vip,town) VALUES(1,2,'Ivanov','Ivanov i i', 'Y', 'Grodno');
+INSERT INTO t_client (id_client,id_dept,moniker,name,is_vip,town) VALUES(2,3,'Sidorov','Sidorov i i', 'N', 'Grodno');
+INSERT INTO t_client (id_client,id_dept,moniker,name,is_vip,town) VALUES(3,3,'Petrov','Petrov i i', 'N', 'Grodno');
 
 
-insert into t_sale (id_sale,num,dt,id_client,id_state,discount) values (1,1,DATE'2018-08-18',1,1,10);
-insert into t_sale (id_sale,num,dt,id_client,id_state,discount) values (2,2,DATE'2018-08-19',2,1,10);
-insert into t_sale (id_sale,num,dt,id_client,id_state,discount) values (3,1,DATE'2018-08-20',3,1,10);
+INSERT INTO t_sale (id_sale,num,dt,id_client,id_state,discount) VALUES (1,1,DATE'2018-08-18',1,1,10);
+INSERT INTO t_sale (id_sale,num,dt,id_client,id_state,discount) VALUES (2,2,DATE'2018-08-19',2,1,10);
+INSERT INTO t_sale (id_sale,num,dt,id_client,id_state,discount) VALUES (3,1,DATE'2018-08-20',3,1,10);
 
-insert into t_sale_str (id_sale_str,id_sale,num,id_ware,qty,discount) values(1,1,1,1,10,2);
-insert into t_sale_str (id_sale_str,id_sale,num,id_ware,qty,discount) values(2,1,2,2,5,2);
-insert into t_sale_str (id_sale_str,id_sale,num,id_ware,qty,discount) values(3,1,3,3,5,2);
-insert into t_sale_str (id_sale_str,id_sale,num,id_ware,qty,discount) values(4,3,4,3,5,2);
+INSERT INTO t_sale_str (id_sale_str,id_sale,num,id_ware,qty,discount) VALUES(1,1,1,1,1,2);
+INSERT INTO t_sale_str (id_sale_str,id_sale,num,id_ware,qty,discount) VALUES(2,1,2,2,2,2);
+INSERT INTO t_sale_str (id_sale_str,id_sale,num,id_ware,qty,discount) VALUES(3,1,3,3,3,2);
+INSERT INTO t_sale_str (id_sale_str,id_sale,num,id_ware,qty,discount) VALUES(4,3,4,3,1,2);
 
 
 
